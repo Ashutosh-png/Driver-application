5cf0521cbacc9c9a96219000a02ebea977d2696f
Ashutosh barwal <rajputashutosh999@gmail.com>
2023-10-06 16:47:29 +0530

    making separate apk

--- android/app/src/debug/java/com/ashutosh1239/test_app/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/ashutosh1239/test_app/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.ashutosh1239.test_app;
+package com.ashutosh1239.test_driver;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
--- android/app/src/main/java/com/ashutosh1239/test_app/MainApplication.java
+++ android/app/src/main/java/com/ashutosh1239/test_app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.ashutosh1239.test_app;
+package com.ashutosh1239.test_driver;
 
 import android.app.Application;
 import android.content.res.Configuration;
